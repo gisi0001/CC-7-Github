10cd7586706782846f46199126d52d09d1a8518f
SIMPSON Gillian <s0817452@ed.ac.uk>
2017-09-26 16:44:11 +0100

    Gill - first trying to make soil T/elevation plot

--- Sample_script.R
+++ Sample_script.R
@@ -4,16 +4,17 @@
 #                                                          #
 ############################################################
 
-# Add in your name
-# Add in purpose of this script
+# Gill
+# Learning github course
 
 # Libraries ----
 
 # Functions ----
 # This is a ggplot function for a nice clean theme
 # HINT: use this theme when making your plot later on
+
 theme.clean <- function(){
-  theme_bw()+
+  theme_bw() +
     theme(axis.text.x=element_text(size=12, angle=45, vjust=1, hjust=1),
           axis.text.y=element_text(size=12),
           axis.title.x=element_text(size=14, face="plain"),             
@@ -30,8 +31,18 @@ theme.clean <- function(){
 }
 
 # Load data ----
+library(readr)
+temp_elevation <- read_csv("M:/GIT practice/CC-7-Github/temp_elevation.csv")
+
+which(is.na(temp_elevation$Soil.temp.mean))
+na.omit
 
 # Make a plot showing how soil temperature changes with elevation ----
+(temp.el <- ggplot (temp_elevation, aes(x = Elevation.m, y = Soil.temp.mean)) +
+   geom_point(colour = "#8B4513") +
+   geom_smooth(method = lm, colour = "#8B4513", fill = "#8B4513", alpha = 0.6) +
+   labs(x = "Elevation (m)", y = "Mean soil temperature (°C)") +
+   theme.clean())
 
 # Save your plot in your project directory
 
